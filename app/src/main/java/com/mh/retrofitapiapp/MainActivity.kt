b39b23375d65e86d3cdb2034d852814bfbb8f63f
Marcelo Hirota <marcelohirota@gmail.com>
2023-05-11 16:42:44 +0930

    Fetch response from API

--- app/src/main/java/com/mh/retrofitapiapp/MainActivity.kt
+++ app/src/main/java/com/mh/retrofitapiapp/MainActivity.kt
@@ -2,10 +2,42 @@ package com.mh.retrofitapiapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.TextView
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.Observer
+import androidx.lifecycle.liveData
+import retrofit2.Response
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val textview = findViewById<TextView>(R.id.textView)
+
+        val retrofitService = RetrofitInstance
+            .getRetrofitInstance()
+            .create(AlbumService::class.java)
+
+        val responseLiveData: LiveData<Response<Albums>> = liveData {
+            val response = retrofitService.getAlbums()
+            emit(response)
+        }
+
+        responseLiveData.observe(this, Observer {
+            val albumsList = it.body()?.listIterator()
+
+            if (albumsList != null) {
+                while (albumsList.hasNext()) {
+                    val albumsItem = albumsList.next()
+                    // Log.i("TAGY", albumsItem.title)
+                    val result = "Album Title : ${albumsItem.title} \n"
+
+
+                    textview.append(result)
+                }
+            }
+        })
     }
 }
--- app/src/main/java/com/mh/retrofitapiapp/RetrofitInstance.kt
+++ app/src/main/java/com/mh/retrofitapiapp/RetrofitInstance.kt
@@ -1,8 +1,20 @@
 package com.mh.retrofitapiapp
 
+import com.google.gson.GsonBuilder
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+
 class RetrofitInstance {
 
     companion object {
         val BASE_URL: String = "https://jsonplaceholder.typicode.com"
+
+        fun getRetrofitInstance(): Retrofit {
+            return Retrofit.Builder()
+                .baseUrl(BASE_URL)
+                .addConverterFactory(GsonConverterFactory.create(
+                    GsonBuilder().create()))
+                .build()
+        }
     }
 }
